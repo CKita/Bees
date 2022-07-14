61d445da7a4a17b93741e21dddc4302b6acac99b
Cristina A. Kita <c.akemikita@gmail.com>
2022-07-14 12:58:37 -0300

    Tutorial
    passei para o inglês

--- survival/Code/effect_sizes_model_survival.R
+++ survival/Code/effect_sizes_model_survival.R
@@ -1,39 +1,68 @@
-#script tamanhos de efeito e modelo - sobrevivencia de abelhas 
+##############################################
 
+# Bee survival  
+
+##############################################
+
+
+#Let's get ready for running the code
+
+#Set the working directory that contains the bee survival data.  
+
+#Delete all previous objects
+
+rm(list= ls())
+
+
+##############################################
+##### EFFECT SIZES 
+##############################################
+
+#Now, let's see our the raw data set
 
 dat <- read.csv("dados_sobrevivencia.csv", h= T, dec =".", sep = ",")
 
 View(dat)
 
-#Calculando os tamanhos de efeito####
+#To calculate the effect sizes, Load the required package: 
 
-#abrindo o pacote necessario
 library(metafor)
 
-#como temos os dados na forma de tabela de contigencia, vamos usar odds ratio para calcular a probabilidade de sobrevivencia 
-#das abelhas 
-#para isso, a gente usa funcao escalc e indica que queremos o resultado em odds ratio ("OR")
+#To calculate the effect sizes, we are going to use the "escalc" function.
+#To calculate the bee survival probability, we are going to use odds ratio ("OR") as a metric of effect size.
 
 effects_t <- escalc("OR", dat$ai,dat$bi, dat$ci, dat$di, dat$n1i, dat$n2i)
 
 View(effects_t)
 
+#Now, let's bind the effect sizes with the raw data by columns 
+
 dat_comp <- cbind(dat, effects_t)
 
 View(dat_comp)
 
-#salvando
-write.csv2(dat_comp, "data_comp.csv", row.names = F) #estou salvando assim para fazer a matriz de covariancia filogenetica
+#OK. Let's save the complete data set
 
-write.csv(dat_comp, "data_comp_2.csv", row.names = F)
+write.csv2(dat_comp, "data_comp.csv", row.names = F) 
 
-#agora abrindo a planilha certa
+
+
+##############################################
+####MODEL
+##############################################
+
+##We are going to build a meta-analytic model to calculate the mean effect size.
+#let's use the complete data set to build our model.  
 
 dados <- read.table("data_comp.csv", h=T, dec=",", sep = ";")
 
 View(dados)
 
-#criando matrix de covariancia filogenetica para inserir como variavel aleatoria
+#Considering that bee survival rate is correlated with life history, we built a phylogenetic covariance matrix
+#using the Interactive Tree of Life online tree generator database. 
+#So, let's do this
+
+#install the packages: 
 
 install.packages("scales")
 install.packages("ape")
@@ -43,14 +72,14 @@ library(scales)
 library(ape)
 library(rotl)
 
+#Let's search our bee species in the Tree of Life 
 
-spp <- tnrs_match_names(unique(dados$bee_specie), context_name = "Animals") #aqui para procurar os nomes das minhas spp de abelhas
+spp <- tnrs_match_names(unique(dados$bee_specie), context_name = "Animals") 
 
-#na categoria "Animals" #do Tree of Life
+my_tree = tol_induced_subtree(ott_ids=spp$ott_id) #here, we are creating a sub tree with our bee species. 
 
-my_tree = tol_induced_subtree(ott_ids=spp$ott_id) #aqui estou criando uma sub arvore com as minhas especies de abelhas 
+#Now we are calculating the phylogenetic distances and creating the covariance matrix 
 
-#daqui para baixo estou vendos as distancias filogeneticas e criando a matriz de covariancia 
 otl_tips=strip_ott_ids(my_tree$tip.label, remove_underscores=TRUE)
 
 taxon_map=structure(spp$search_string, names=spp$unique_name)
@@ -59,40 +88,35 @@ my_tree$tip.label=taxon_map[otl_tips]
 
 my_tree.ult = compute.brlen(my_tree, method = "Grafen")
 
-windows()
-plot(my_tree.ult,no.margin=T) #vamos ver como ficou
+plot(my_tree.ult,no.margin=T) #let's see
 
 cov.matrix = vcv(my_tree.ult,corr=T)
 cov.matrix[,0]
 
-#Montando o modelo####
+#OK. Now, let's build our model
 
-library(metafor)
+#We are going to build a meta-analytic mixed-effects model.
+#We are going to use the function rma.mv from the metafor package because of the non-independence of our data. 
 
-survival <- rma.mv(yi, vi, random = list( ~1|id_code, ~1|bee_specie, ~1|agrochemical, ~1|hours_after_exposure, ~1|exposure_type), method="REML",  # "REML" = multi-level 
-            R = list(bee_specie = cov.matrix),digits = 3, data = dados)   #aqui que entra a matriz de covariancia filogenetica                      
-
+survival <- rma.mv(yi, vi, random = list( ~1|id_code, ~1|bee_specie, ~1|agrochemical, ~1|hours_after_exposure, ~1|exposure_type), method="REML",  #"REML" = multi-level 
+            R = list(bee_specie = cov.matrix),digits = 3, data = dados)                   
 
 summary(survival)
 
-str(survival)
 
-###Grafico####
+#Let's see the result in a graphic 
 
-#instalando pacotes:
+#install the required packages:
 
 install.packages("dplyr")
-
 install.packages("devtools")
 install.packages("tidyverse")
-install.packages("metafor")
 install.packages("patchwork")
 install.packages("R.rsp")
 install.packages("ggplot2")
 install.packages("Rtools42")
 
 devtools::install_github("itchyshin/orchard_plot", subdir = "orchaRd", force = TRUE, build_vignettes = TRUE)
-
 
 library(devtools)
 library(orchaRd)
@@ -101,6 +125,7 @@ library(tidyverse)
 library(metafor)
 
 
+#Let's generate a figure .tiff  
 
 tiff('survival.tiff', units="in", width=5, height=6, res=1200, compression = 'lzw')
 
@@ -117,36 +142,35 @@ orchard_plot(survival, xlab = "Odds ratio") +
 
 dev.off()
 
-#vamos fazer um teste de sensibilidade porque temos pontos extremos 
+#We can see that some effect sizes are extreme comparing to the mean effect size.
+#So, let's do a sensibility test to analyse the influence of these effect sizes.
 
-#teste de sensibilidade ####
+#here, the sensibility test that we are going to do is evaluating the standardized residuals   
 
-rs= rstandard(survival) #para ver se tem outliers 
-rs
-plot(rs$resid, ylim = c(-8.0,8), xlim =c(-5,50))
-text(rs$resid, labels = dados$id_code, cex= 1, pos = 2)
+#Calculating the standardized residuals
+rs = rstandard(survival) 
+plot(rs$resid, ylim = c(-8.0,8), xlim =c(-8,50))
 
+#If our residuals are +/-3, then it means that something extremely unusual is happening. 
 abline(h = -3)
 abline(h = 3)
 
-# Os pontos C0073, C0084, C0189 e C0191  são outliers (28 pontos). 
-# Podemos observar melhor esses pontos vendo o gráfico.  
+text(rs$resid, labels = dados$id_code, cex= 1, pos = 2)
 
-#vamos fazer um modelo sem os outliers
+#So, the effect sizes C0073, C0084, C0189 e C0191  are outliers (28 effect sizes). 
+
+#Let's build a models without outliers 
 
 surv_sensi2<- read.csv("survival_sensi_out2.csv", h= T, dec =".", sep = ",")
 
 View(surv_sensi2)
 
-##### construindo a matriz de covariancia
+#Building a covariance matrix using only the bee species of our data set without outliers 
 
-spp <- tnrs_match_names(unique(surv_sensi2$bee_specie), context_name = "Animals") #aqui para procurar os nomes das minhas spp de abelhas
+spp <- tnrs_match_names(unique(surv_sensi2$bee_specie), context_name = "Animals") 
 
-#na categoria "Animals" #do Tree of Life
+my_tree = tol_induced_subtree(ott_ids=spp$ott_id)
 
-my_tree = tol_induced_subtree(ott_ids=spp$ott_id) #aqui estou criando uma sub arvore com as minhas especies de abelhas 
-
-#daqui para baixo estou vendos as distancias filogeneticas e criando a matriz de covariancia 
 otl_tips=strip_ott_ids(my_tree$tip.label, remove_underscores=TRUE)
 
 taxon_map=structure(spp$search_string, names=spp$unique_name)
@@ -155,18 +179,19 @@ my_tree$tip.label=taxon_map[otl_tips]
 
 my_tree.ult = compute.brlen(my_tree, method = "Grafen")
 
-windows()
-plot(my_tree.ult,no.margin=T) #vamos ver como ficou
+plot(my_tree.ult,no.margin=T) 
 
 cov.matrix = vcv(my_tree.ult,corr=T)
 cov.matrix[,0]
 
-#####
+#our model without outliers 
 
 model.surv.sensi.out2 <- rma.mv(yi, vi, random = list( ~1|id_code, ~1|bee_specie, ~1|agrochemical, ~1|hours_after_exposure, ~1|exposure_type), method="REML",  # "REML" = multi-level 
                                 R = list(bee_specie = cov.matrix),digits = 3, control = list(optimizer="optim", optmethod="Nelder-Mead"), data = surv_sensi2)
 
 summary(model.surv.sensi.out2)
+
+tiff('survival_sensibility_test.tiff', units="in", width=5, height=6, res=1200, compression = 'lzw')
 
 orchard_plot(model.surv.sensi.out2, xlab = "Odds ratio") +
         labs(y = "Survival") + # troca o nome do eixo y
@@ -179,31 +204,34 @@ orchard_plot(model.surv.sensi.out2, xlab = "Odds ratio") +
               legend.position = "top") + #troca a posicao da legenda 
         coord_flip() 
 
-#negativo e diferente de zero. Ou seja, os pesticidas afetam a sobrevivenciua das abelhas.
-#isso indica que o efeito do agrotóxico é forte o bastante para não deixar de ser detectado mesmo quando o tamanho amostral é menor.
+dev.off()
+
+#Result: mean effect size is negative and different from zero. In other words, pesticide effects are strong enough to be detected
+#even when our sampling size is reduced.  
 
 
-###Heterogeneidades####
+###############################################
+####HETEROGENEITY
+###############################################
+
+#Now, we are going to calculate the heterogeneity the model and the heterogeneity of each random variable,
+#using the IÂ² statistic with a 95% confidence interval (CI).
+
 
 dados$wi <- 1/sqrt(dados$vi) # precision = 1 / standard error of effect size (Equation 20; Nakagawa & Santos 2012)
 
 s2m.0 <- sum(dados$wi*(length(dados$wi)-1))/(sum(dados$wi)^2-sum(dados$wi^2)) # Equation 22
 
+#Let's calculate the total heterogeneity. In other words, the heterogeneity that is explained by our model 
+#including predictor and response variables
 
-#vamos calcular a heterogeneidade TOTAL, ou seja, a heterogeneidade explicada pelo modelo
-#incluindo preditores e variaveis respostas 
-
-I2.survival <- ((survival$sigma2[1] + survival$sigma2[2] +  survival$sigma2[3] + survival$sigma2[4] + survival$sigma2[5])/ #[1] porque eu tenho o fator aleatorio id_code e [2] pq tem o fator aleatoria bee_specie. 
+I2.survival <- ((survival$sigma2[1] + survival$sigma2[2] +  survival$sigma2[3] + survival$sigma2[4] + survival$sigma2[5])/  
                  (survival$sigma2[1] + survival$sigma2[2] +  survival$sigma2[3] + survival$sigma2[4] + survival$sigma2[5] + s2m.0)) * 100 
 
-#o sigma foi dado no summary do modelo. Ele indica a heterogenidade, mas de 
-#forma nao intuitiva. Entao a gente calcula o I2 porque a heterogenidade pode
-#pode ser interpretada como uma porcentagem 
+#The values of heterogeneity 25%, 50%, and 75% 
+#were considered as small, medium, and high, respectively, as suggested by Higgins (2003).
 
-I2.survival #heterogeneidade media
-
-#meu modelo tem 90% de heterogeneidade. 
-#em ecologia, geralemnte a heterogeneidade > 70%
+I2.survival #mean 
 
 
 ## and 95% CI for I2.total:
@@ -211,7 +239,7 @@ I2.survival #heterogeneidade media
 I2.survival - qchisq(.95, df=1)/2; I2.survival + qchisq(.95, df=1)/2
 
 
-#Calculando I² para cada fator aleatorio
+#Calculating IÂ² for each random factor 
 
 #--- study ID ---
 
@@ -221,7 +249,7 @@ I2.study.bee <- ((survival$sigma2[1])/(
 
 I2.study.bee
 
-## and 95% CI for I2.paper:
+## and 95% CI for I2.study.bee:
 
 I2.study.bee - qchisq(.95, df=1)/2; I2.study.bee + qchisq(.95, df=1)/2
 
@@ -233,20 +261,21 @@ I2.bee <- ((survival$sigma2[2])/(
 
 I2.bee
 
-## and 95% CI for I2.paper:
+## and 95% CI for I2.bee:
 I2.bee - qchisq(.95, df=1)/2; I2.bee + qchisq(.95, df=1)/2
 
 #--- agrochemical ---
+
 I2.agro<- ((survival$sigma2[3])/(
         survival$sigma2[1]+survival$sigma2[2] +  survival$sigma2[3] + survival$sigma2[4] + survival$sigma2[5] +
                 s2m.0)) * 100
 
 I2.agro
 
-## and 95% CI for I2.paper:
+## and 95% CI for I2.agro:
 I2.agro - qchisq(.95, df=1)/2; I2.agro + qchisq(.95, df=1)/2
 
-#----hours
+#----hours ---
 
 I2.hours<- ((survival$sigma2[4])/(
         survival$sigma2[1]+survival$sigma2[2] +  survival$sigma2[3] + survival$sigma2[4] + survival$sigma2[5] +
@@ -254,7 +283,7 @@ I2.hours<- ((survival$sigma2[4])/(
 
 I2.hours
 
-## and 95% CI for I2.paper:
+## and 95% CI for I2.hours:
 I2.hours - qchisq(.95, df=1)/2; I2.hours + qchisq(.95, df=1)/2
 
 #---exposure type
@@ -265,22 +294,26 @@ I2.exp<- ((survival$sigma2[5])/(
 
 I2.exp
 
-## and 95% CI for I2.paper:
+## and 95% CI for I2.exp:
 I2.exp - qchisq(.95, df=1)/2; I2.exp + qchisq(.95, df=1)/2
 
-#intervalos de confiaca sigma2
+#confidence intervals for sigma2
 
 confint(survival)
 
-#Vies de publicacao####
 
-#Publication bias (egger)
-#Intercept significativo indica que ha vies de publicacao
+#############################################
+####PUBLICATION BIAS
+#############################################
 
+
+#Publication bias in each model using an adapted version of Eggerâ€™s regression (Nakagawa & Santos, 2012)
+
+#A publication bias is pointed out when the intercept of the regression significantly deviates from zero
 
 eggsm3 = lm(residuals(survival)~sqrt(dados$vi)) 
 
-summary(eggsm3) #tem vies de publicacao pq o intercepto deu diferente de zero e deu significativo 
+summary(eggsm3) 
 
 confint.lm(eggsm3, level = 0.95)
 
